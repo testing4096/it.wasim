be44cb425a1b6ea58350bd9af3e40ca628adf578
testing4096 <141908836+testing4096@users.noreply.github.com>
2023-11-30 10:53:19 +0530

    Add files via upload

---
+++ orange-Selenium-java-wd-junit.java
@@ -0,0 +1,83 @@
+package com.example.tests;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Orange {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    baseUrl = "https://www.blazedemo.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testOrange() throws Exception {
+    // Label: Test
+    // ERROR: Caught exception [ERROR: Unsupported command [resizeWindow | 1011,651 | ]]
+    driver.get("http://183.82.103.245/nareshit/login.php");
+    driver.findElement(By.name("txtUserName")).click();
+    driver.findElement(By.xpath("//input[@value = \"Login\"]")).click();
+    driver.findElement(By.name("txtUserName")).click();
+    driver.findElement(By.xpath("//input[@value = \"Login\"]")).click();
+    driver.findElement(By.name("txtUserName")).click();
+    // ERROR: Caught exception [unknown command [typeSecret]]
+    driver.findElement(By.name("txtUserName")).clear();
+    driver.findElement(By.name("txtUserName")).sendKeys("nareshit");
+    driver.findElement(By.xpath("//input[@value = \"Login\"]")).click();
+    driver.findElement(By.xpath("//*[text() = \"Logout\"]")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
