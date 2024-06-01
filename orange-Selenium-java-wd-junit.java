c54acdafb49214df97b3c2f4ea46de898e9a3c8c
testing4096 <141908836+testing4096@users.noreply.github.com>
2024-06-01 12:13:15 +0530

    Update orange-Selenium-java-wd-junit.java

--- orange-Selenium-java-wd-junit.java
+++ orange-Selenium-java-wd-junit.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
